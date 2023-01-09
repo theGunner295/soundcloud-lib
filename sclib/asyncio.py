95442599a4f46f2219ebf56cbce7919d5e8d58a6
the_Gunner295 <JJAKruger@Outlook.com>
2023-01-09 16:47:19 +0000

    Changed format of renamed function

--- sclib/asyncio.py
+++ sclib/asyncio.py
@@ -89,7 +89,7 @@ class SoundcloudAPI(sync.SoundcloudAPI):
 
         if obj['kind'] == 'playlist':
             playlist = Playlist(obj=obj, client=self)
-            await playlist.asyncclean_attributes()
+            await playlist.async_clean_attributes()
             return playlist
 
     async def get_tracks(self, *track_ids):  # pylint: disable=invalid-overridden-method
@@ -162,7 +162,7 @@ class Playlist(sync.Playlist):
 
     RESOLVE_THRESHOLD = 100
 
-    async def asyncclean_attributes(self): # pylint: disable=invalid-overridden-method
+    async def async_clean_attributes(self): # pylint: disable=invalid-overridden-method
         """ Async clean_attributes """
         if self.ready:
             return
@@ -190,7 +190,7 @@ class Playlist(sync.Playlist):
         return int(self.track_count)
 
     async def __aiter__(self):
-        await self.asyncclean_attributes()
+        await self.async_clean_attributes()
         for track in self.tracks:
             yield track
 
